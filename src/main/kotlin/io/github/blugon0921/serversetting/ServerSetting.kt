a199536e55a01cb79c7fbcd25856a1966be13e90
Blugon <71944969+Blugon0921@users.noreply.github.com>
2021-10-09 22:55:11 +0900

    ServerSetting

--- src/main/kotlin/io/github/blugon0921/serversetting/ServerSetting.kt
+++ src/main/kotlin/io/github/blugon0921/serversetting/ServerSetting.kt
@@ -6,11 +6,14 @@ import io.github.blugon0921.serversetting.events.ServerPingList
 import net.kyori.adventure.text.Component
 import net.md_5.bungee.api.ChatColor
 import org.bukkit.Bukkit
-import java.awt.Color
+import org.bukkit.command.SimpleCommandMap
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.event.Listener
+import org.bukkit.plugin.SimplePluginManager
 import org.bukkit.plugin.java.JavaPlugin
+import java.awt.Color
 import java.io.File
+import java.lang.reflect.Field
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -21,18 +24,18 @@ class ServerSetting : JavaPlugin(), Listener {
         var yaml = YamlConfiguration.loadConfiguration(configFile)
 
         fun Replace(replaceMessage: String): String {
-            var replaceMessage = replaceMessage
+            var replace_message = replaceMessage
             val date = Date()
-            replaceMessage = replaceMessage.replace("&", "§")
-            replaceMessage = replaceMessage.replace("%random%", ChatColor.of(Color((Math.random()*255).toInt(),(Math.random()*255).toInt(),(Math.random()*255).toInt())).toString()) //RandomColor
-            replaceMessage = replaceMessage.replace("%version%", Bukkit.getServer().minecraftVersion) //Version
-            replaceMessage = replaceMessage.replace("%years%", SimpleDateFormat("yyyy").format(date)) //Years
-            replaceMessage = replaceMessage.replace("%month%", SimpleDateFormat("MM").format(date)) //Month
-            replaceMessage = replaceMessage.replace("%day%", SimpleDateFormat("dd").format(date)) //Days
-            replaceMessage = replaceMessage.replace("%hour%", SimpleDateFormat("HH").format(date)) //Hour
-            replaceMessage = replaceMessage.replace("%minute%", SimpleDateFormat("mm").format(date)) //Minutes
-            replaceMessage = replaceMessage.replace("%second%", SimpleDateFormat("ss").format(date)) //Seconds
-            return replaceMessage
+            replace_message = replace_message.replace("&", "§")
+            replace_message = replace_message.replace("%random%", ChatColor.of(Color((Math.random()*255).toInt(),(Math.random()*255).toInt(),(Math.random()*255).toInt())).toString()) //RandomColor
+            replace_message = replace_message.replace("%version%", Bukkit.getServer().minecraftVersion) //Version
+            replace_message = replace_message.replace("%years%", SimpleDateFormat("yyyy").format(date)) //Years
+            replace_message = replace_message.replace("%month%", SimpleDateFormat("MM").format(date)) //Month
+            replace_message = replace_message.replace("%day%", SimpleDateFormat("dd").format(date)) //Days
+            replace_message = replace_message.replace("%hour%", SimpleDateFormat("HH").format(date)) //Hour
+            replace_message = replace_message.replace("%minute%", SimpleDateFormat("mm").format(date)) //Minutes
+            replace_message = replace_message.replace("%second%", SimpleDateFormat("ss").format(date)) //Seconds
+            return replace_message
         }
     }
 
